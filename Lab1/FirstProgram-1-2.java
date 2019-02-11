98937908293ee7d4c17b153279e028d65216e4e9
pan-jessica <45519331+pan-jessica@users.noreply.github.com>
2019-02-10 19:30:23 -0800

    Add files via upload

---
+++ Lab1/FirstProgram-1-2.java
@@ -0,0 +1,47 @@
+
+public class FirstProgram {
+
+	
+
+	/**
+	 
+	 * @param args
+	 
+	 */
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		// This is a single line comment
+		
+		/*
+		 
+		 * multi lines comment
+		 
+		 * 
+		 
+		 */
+		
+		
+
+		System.out.print ("Hello ");
+		
+		System.out.println ("World!");
+		
+		System.out.println ("My name is Jessica Pan!");
+		
+		System.out.print ("Welcome to CSE20 at UC Merced");
+		
+		
+	
+		return;
+
+	
+	}
+
+
+}
