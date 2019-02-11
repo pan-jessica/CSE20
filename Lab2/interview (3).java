fa9bb63e62e38d3bafa9fbb1ad18385bbc91ba55
pan-jessica <45519331+pan-jessica@users.noreply.github.com>
2019-02-10 19:34:44 -0800

    Add files via upload

---
+++ Lab2/interview (3).java
@@ -0,0 +1,22 @@
+
+public class interview {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.print("Hi, I interviewed Jonathan and ");
+		System.out.println("he likes to watch anime.");
+		System.out.print("He likes ultimate frisbee ");
+		System.out.println("and is favorite color is black.");
+		System.out.println("Jonathan's from south LA!");
+		System.out.print("His favorite class so far is this class.");
+		
+		
+		
+		
+
+	}
+
+}
